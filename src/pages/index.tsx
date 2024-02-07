10cb11661a45a0737f874cde808383b2332d2710
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-07 17:26:53 -0300

    :sparkles: feat: adding styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 
 import { Inter } from "next/font/google";
+import styled from "styled-components";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
 export default function Home() {
   return (
-    <>
-     <h1 className={inter.className}>Hello World</h1>
-    </>
+    <div className={inter.className}>
+      <Title>Hello World</Title>
+    </div>
   );
 }
