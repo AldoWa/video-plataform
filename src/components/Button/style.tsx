6c0ff06a7547e9b0b327769220c2cdf3c0cf1808
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-09 12:35:14 -0300

    :sparkles: feat: adding icons

--- src/components/Button/style.tsx
+++ src/components/Button/style.tsx
@@ -1,5 +1,4 @@
 import styled, { css } from "styled-components";
-
 export const Button = styled.button<{ bg: 'transparent' | 'green'}>`
   ${({ bg, theme }) => css`
     background-color: ${bg === 'green' ? 'green' : 'transparent'};
@@ -14,10 +13,22 @@ export const Button = styled.button<{ bg: 'transparent' | 'green'}>`
     font-size: ${theme.font.sizes[14]};
     border-radius: 4px;
     border: ${bg === 'transparent' ? `1px solid ${theme.color.product.blue}` : 'none'};
+    & svg {
+      margin-right: 0.625rem;
+      & path {
+        stroke: ${bg === 'transparent' ? theme.color.product.blue : theme.color.base.white};
+      }
+      font-size: 1.25rem;
+    }
     &:hover {
       background-color: ${bg === 'green' ? theme.color.product.green_dark : theme.color.product.blue};
       transition: background-color 0.3s ease-in-out;
       color: ${bg === 'transparent' ? theme.color.base.background : theme.color.base.white};
+      & svg {
+        & path {
+          stroke: ${bg === 'transparent' ? theme.color.base.background : theme.color.base.white};
+        }
+      }
     }
   `}
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,17 @@
 import {  Roboto } from "next/font/google";
 import Button from '@/components/Button'
 
+import Discord from '@/assets/icons/discord.svg'
+
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
 export default function Home() {
   return (
     <div className={roboto.className}>
-      <Button>Comunidade no discord</Button>
+      <Button >
+        <Discord />
+        Comunidade no discord
+      </Button>
     </div>
   );
 }
